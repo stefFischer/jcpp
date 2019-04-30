37aca00cda36cb8073172d5e88e5d3206e63daad
Stefan Fischer <Stefan.Fischer@jku.at>
2019-04-30 17:08:03 +0200

    added some API for PreProcessor usage

---
+++ processed/MinTest.c
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+//#if MIN(2, 3) > 1
+
+//#if ((2) < (3) ? (3) : (2)) < 10
+//#if 2 < 10
+int a;
+
+
+int main(){
+	
+	int a1 = 1;
+	int a2 = 4;
+	
+	printf("MIN: %d - ", ({__typeof__(a1) _a = (a1); __typeof__(a2) _b = (a2); _a < _b ? _a : _b;}));
+	
+	printf("MAX: %d - ", ((a1) < (a2) ? (a2) : (a1)));
+	
+	
+ 
+	int x = 10;
+	int y = 5;
+	int z = 4;
+ 
+	// What happens now?
+	if(x < 0)
+	    { x ^= y; y ^= x; x ^= y; };
+	else
+	    { x ^= z; z ^= x; x ^= z; }; 
+	
+	
+}
+
